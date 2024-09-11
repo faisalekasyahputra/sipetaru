f9d7399ad52be8e145c97eabda9f7367950c6313
g1br4n <paisaleee@gmail.com>
2024-09-11 11:11:51 +0700

    Update peta.php

--- application/views/peta/peta.php
+++ application/views/peta/peta.php
@@ -13,113 +13,465 @@
 
 </div>
 </div>
+<style>
+    html,
+    body {
+        height: 100%;
+        margin: 0;
+        overflow: hidden;
+    }
 
+    #map {
+        width: auto;
+        height: 92%;
+        position: relative;
+        z-index: 1;
+    }
+
+
+
+    h6 {
+
+        color: #fff;
+
+        padding: 0px;
+
+    }
+
+    .info {
+
+        width: 300px;
+
+        height: auto;
+
+        max-height: 750px;
+
+        min-height: 370px;
+
+        overflow-y: scroll;
+
+        scrollbar-color: rebeccapurple green;
+
+        font-family: 'Poppins', sans-serif;
+
+        background: white;
+        background: rgba(255, 255, 255, 0.8);
+
+        box-shadow: 0 0 15px rgba(0, 0, 0, 0.2);
+
+        border-radius: 5px;
+
+        display: block;
+
+        position: relative;
+
+        z-index: 2;
+    }
+
+    .attr {
+
+        width: 250px;
+
+        max-width: 250px;
+
+        height: auto;
+
+        max-height: 750px;
+
+        min-height: 170px;
+
+        z-index: 100;
+
+        font-family: 'Poppins', sans-serif;
+
+        background: white;
+        background: rgba(255, 255, 255, 1);
+
+        box-shadow: 0 0 15px rgba(0, 0, 0, 0.2);
+
+        border-radius: 5px;
+
+        display: block;
+
+        position: relative;
+
+        z-index: 2;
+    }
+
+    .atas {
+
+        width: 284px;
+
+        height: 30px;
+
+        padding: 6px 8px;
+
+        font-family: 'Poppins', sans-serif;
+
+        background: rgba(0, 145, 168, 0.8);
+
+        box-shadow: 0 0 15px rgba(0, 0, 0, 0.2);
+
+        vertical-align: text-top;
+
+        border-radius: 2px;
+    }
+
+    .ataspencarian {
+
+
+
+        position: relative;
+
+        display: flex;
+
+        vertical-align: middle;
+
+        text-align: center;
+
+        background: rgba(0, 145, 168, 0.8);
+
+        height: 30px;
+
+        padding: 6px 8px;
+
+
+
+        align-items: baseline;
+
+        justify-content: center;
+
+    }
+
+    .box_basemap {
+
+
+
+        margin: 10px 5px 5px 10px;
+
+        border-radius: 5px;
+
+    }
+
+    .center {
+
+        margin: 0;
+
+        position: absolute;
+
+        justify-content: center;
+
+        align-items: center;
+
+    }
+
+    .drop {
+        padding-top: 10px;
+        margin-left: 250px
+    }
+
+    #from_admin {
+        display: none;
+    }
+
+    #from_tematik {
+        display: none;
+    }
+
+    #from_rencana {
+        display: none;
+    }
+
+    #from_rencana2 {
+        display: none;
+    }
+
+    #from_renta {
+        display: none;
+    }
+
+
+
+    .attrhead {
+        width: 250px;
+
+
+
+        position: relative;
+
+        display: inline-block;
+
+        vertical-align: middle;
+
+        text-align: center;
+
+        background: rgba(0, 145, 168, 0.8);
+
+        box-shadow: 0 0 15px rgba(0, 0, 0, 0.2);
+
+
+
+
+
+    }
+
+    .label_kec {
+        width: auto;
+        height:
+
+            auto;
+        font-size: 12px;
+
+        border-color: none;
+
+        border-width: 0;
+
+        background: rgba(255, 255, 255, 0.0);
+
+        font-weight: bold;
+
+        text-shadow: -1px 0 red, 0 1px black, 1px 0 red, 0 -1px red;
+
+        color: white;
+    }
+
+
+
+    .leaflet-control-dialog {
+
+        position: absolute;
+
+        background: white;
+        background: rgba(255, 255, 255, 1);
+
+        padding: 0px;
+
+        text-align: center;
+
+        border-radius: 4px;
+
+        box-shadow: 0 1px 5px rgba(0, 0, 0, 0.65);
+
+    }
+
+    .panelatas {
+
+        /* width:384px; */
+
+        /* height: 10px; */
+
+        padding: 3px 0px;
+
+        font-family: 'Poppins', sans-serif;
+
+        background: rgba(0, 145, 168, 0.8);
+
+        box-shadow: 0 0 15px rgb(0 0 0 / 20%);
+
+        vertical-align: text-top;
+
+        border-radius: 2px;
+
+    }
+
+    .panelcenter {
+
+
+
+        padding: 7px;
+
+        /* position: relative; */
+
+        display: flex;
+
+        vertical-align: middle;
+
+
+
+        align-items: center;
+
+        justify-content: center;
+
+    }
+
+    .leaflet-control-dialog-close {
+
+        position: absolute;
+
+        width: 20px;
+
+        height: 20px;
+
+        top: 0px;
+
+        right: 0px;
+
+        padding: 2px;
+
+        font-size: 16px;
+
+        line-height: 16px;
+    }
+
+    .isi {
+        padding: 20px 15px;
+    }
+
+    .sticky-header1 {
+
+        background-color: #0091a8;
+
+        color: #000000;
+
+        display: flex;
+
+
+
+
+
+        align-items: center;
+
+
+
+    }
+
+    .content-scroll1 {
+
+        margin-top: 0px;
+
+        height: 100%;
+
+
+
+    }
+
+    .banner {
+
+
+
+        display: inline;
+
+        margin-top: 10px;
+
+        margin-left: 15px;
+
+        margin-bottom: 10px;
+
+
+
+
+
+    }
+
+    .banner1 {
+
+
+
+        display: inline;
+
+        margin-left: 10px;
+
+        font-weight: bold;
+
+
+
+
+
+
+
+    }
+
+    .banner2 {
+
+        color: black;
+
+        margin-top: 31px;
+
+        display: inline;
+
+        margin-left: 15px;
+
+        width: 1100px;
+
+        position: relative;
+
+        text-align: right;
+
+    }
+
+    .dinas1 {
+
+        font-size: 28px;
+
+        color: white;
+
+
+
+    }
+
+    .dinas2 {
+
+
+
+        font-size: 22px;
+
+        color: white;
+
+    }
+
+    .pencarian {
+
+        width: 250px;
+
+        max-width: 250px;
+
+        min-height: 90px;
+
+        z-index: 100;
+
+        font-family: 'Poppins', sans-serif;
+
+        background: white;
+        background: rgba(255, 255, 255, 1);
+
+        box-shadow: 0 0 15px rgba(0, 0, 0, 0.2);
+
+
+
+        display: block;
+
+        position: relative;
+
+        z-index: 2;
+    }
+</style>
 <script>
-    var osm = L.tileLayer('https://{s}.tile.osm.org/{z}/{x}/{y}.png', {});
-    var Esri_WorldImagery = L.tileLayer(
-        'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'
-    );
+    peta1 = new L.TileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
 
-    var googleStreets = L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
+    peta2 = L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
+
         maxZoom: 20,
         subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
     });
 
-    var googleHybrid = L.tileLayer('http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}', {
+    peta3 = L.tileLayer('http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}', {
+
         maxZoom: 20,
         subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
     });
-    var googleSat = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
+
+    peta4 = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
         maxZoom: 20,
         subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
     });
 
-    var baseLayers = {
-        "Google Roadmap": googleStreets,
-        "Google Hybrid": googleHybrid,
-        "Google Satellite": googleSat,
-        "Open Street Map": osm,
-        "ESRI World Imagery": Esri_WorldImagery,
-    };
-
-
-    function marker_nya(feature, latlng) {
-
-        var Url_nya = "<?= base_url('assets/ico/lamp1.png') ?>";
-        var Icon = L.icon({
-            iconUrl: Url_nya,
-            iconSize: [50, 50],
-        });
-        var marker = L.marker(latlng, {
-            icon: Icon
-        });
-        return marker
-    }
-
-    function marker_meteran(feature, latlng) {
-
-        var Url_nya = "<?= base_url('assets/ico/meteran.png') ?>";
-        var Icon = L.icon({
-            iconUrl: Url_nya,
-            iconSize: [40, 110],
-        });
-        var marker = L.marker(latlng, {
-            icon: Icon
-        });
-        return marker
-    }
-    var pju = L.geoJSON([<?= $pju ?>], {
-        onEachFeature: popup,
-        pointToLayer: marker_nya,
-        filter: function(feature, layer) {
-            return feature.properties.show_on_map;
-        }
-    });
-    var meteran = L.geoJSON([<?= $meteran ?>], {
-        pointToLayer: marker_meteran,
-        onEachFeature: popup_meteran,
-        filter: function(feature, layer) {
-            return feature.properties.show_on_map;
-        }
-    });
     var map = L.map('map', {
         center: [-6.7521643, 111.0816679],
         zoom: 13,
-        fullscreenControl: true,
-        fullscreenControlOptions: {
-            position: 'topleft'
-        },
-        layers: [googleSat, pju, meteran]
+        maxZoom: 25,
+        minZoom: 12,
+        attributionControl: false,
+        zoomControl: false,
     });
-    var groupedOverlays = {
-        "Peta PJU": {
-            "&nbsp;&nbsp;&nbsp;&nbsp;PJU": pju,
-            "&nbsp;&nbsp;&nbsp;&nbsp;Meteran": meteran,
-        },
-    };
-
-    function popup(f, l) {
-        var out = [];
-        if (f.properties) {
-            var foto = f.properties['Foto'];
-            out.length = 0;
-            out.push("<center><img class='img-thumbnail' src='<?= base_url("assets/foto") ?>/" + foto + "' width='180px' ></center>");
-            out.push('Nama Ruas : ' + f.properties['Nama Ruas']);
-            out.push('Koordinat : ' + f.properties['Koordinat']);
-            out.push("<center><a href='<?= base_url("index.php/Welcome/detail/") ?>" + f.properties['Kode'] + "'  target='blank'>" + 'Lihat Detail' + "</a></center>");
-            l.bindPopup(out.join("<br />"));
-        }
-    };
-
-    function popup_meteran(f, l) {
-        var out = [];
-        if (f.properties) {
-            var foto = f.properties['Foto1'];
-            out.length = 0;
-            out.push("<center><img class='img-thumbnail' src='<?= base_url("assets/foto") ?>/" + foto + "' width='180px' ></center>");
-            out.push('Nomer  : ' + f.properties['Kode']);
-            out.push('Koordinat : ' + f.properties['Koordinat']);
-            l.bindPopup(out.join("<br />"));
-        }
-    };
+    map.addLayer(peta3);
 </script>
