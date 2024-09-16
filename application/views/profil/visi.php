1d2b7dd8fc48bf6cfc5d7432d1240f25ae35cd8d
paesol <102522046+faisalekasyahputra@users.noreply.github.com>
2024-09-16 12:44:01 +0700

    well

---
+++ application/views/profil/visi.php
@@ -0,0 +1,34 @@
+<div class="container mt-3">
+    <div class="row d-flex flex-column garistepi glass p-2 mb-2">
+        <div class="col d-flex justify-content-center">
+            <i class="fa-solid fa-crosshairs fs-2 me-2"></i>
+            <h3 class="m-0 fw-bolder">VISI DAN MISI</h3>
+
+        </div>
+        <div class=" col text-center">
+
+            <small class="m-0">Dinas Pekerjaan Umum dan Penataan Ruang Kabupaten Pati</small>
+
+        </div>
+    </div>
+    <div class="row garistepi glass">
+        <div class="col">
+            <li class="list-group-item glass">
+                <h4>visi</h4>
+            </li>
+            <ol class="list-group list-group-numbered ">
+
+                <li class="list-group-item glass">A list item</li>
+            </ol>
+
+
+            VISI :
+            "Meningkatkan Kesejahteraan Masyarakat dan Pelayanan Publik"
+            MISI :
+            1. Meningkatkan Pemberdayaan Masyarakat Sebagai Upaya Penegentasan Kemiskinan
+            2. Meningkatkan Tata Kelola Pemerintah yang Akuntabel dan Mengutamakan Pelayanan Publik
+            3. Meningkatkan Pembangunan Infrastruktur Daerah, Mendukung Pengembangan Ekonomi Daerah
+
+        </div>
+    </div>
+</div>
